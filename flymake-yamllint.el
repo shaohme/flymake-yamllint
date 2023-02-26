0134f9f864749f30f8ea3c6a86865b35d4352cea
Martin Kjær Jørgensen <me@lagy.org>
2023-02-26 11:26:30 +0100

    bump version

--- flymake-yamllint.el
+++ flymake-yamllint.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Martin Kjær Jørgensen <mkj@gotu.dk>
 ;; Created: 26 November 2021
-;; Version: 0.1.4
+;; Version: 0.1.5
 ;; Package-Requires: ((emacs "26.1"))
 ;; URL: https://github.com/shaohme/flymake-yamllint
 ;;; Commentary:
